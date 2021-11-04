73feb20662f8836785c84f10d4c91097402e230c
Ken Dang <k3ndang@gmail.com>
2021-11-04 13:34:25 -0500

    completed assignment

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,31 +1,41 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
 let firstName = 'Ken';
+console.log('first name:', firstName);
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
 let lastName = 'Dang';
+console.log('last name:', lastName);
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 // (remember, you already have variables for this, can you use those?)
 let fullName = firstName + lastName;
+console.log('full name:', fullName);
 // 4 - Console log the value of `fullName`
 console.log(fullName);
 // 5 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
 let luckyNumber = 13;
+console.log('my lucky number is ' + luckyNumber);
 // 6 - Console log this sentence, adding in the variables you created above:
 // 'My name is (full name), and I think (lucky number) is a winner!'.
 // Refer back to the videos if you need help with this one.
 console.log('My name is', fullName, 'and I think', luckyNumber, 'is a winner!');
 // 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
 let adventurous = true;
+console.log(adventurous);
 // 8 - Create a variable named `food`, and set its value to a string of your favorite food
 let food = 'steak';
+console.log('my favorite food is:', food);
 // 9 - Create a variable called `pets` and set it to the value of the number of pets you have
 let pets = 2;
+console.log('I have ' + pets + ' fishes.');
 // 10 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
 let friendsPets = 2
+console.log('my friend have', friendsPets, 'cats.');
 // 11 - Add two pets to your `pets` variable
-pets += 1;
+pets += 1;  // I just bought another clown fish.
+console.log('pets numbers now are:', pets);
 // 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
 let allowedPets = 4;
+console.log('numbers of pets allow in my car:', allowedPets);
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!",
 // if it's not true, console log "How about we stay home?"
 if (allowedPets >= pets + friendsPets) {
